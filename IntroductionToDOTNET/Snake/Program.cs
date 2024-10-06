6224612d56b63ec19a1834040c61165ce2a2478d
User <User@DESKTOP-0TCDSR6>
2024-10-06 14:44:07 +0300

    Arrays

--- IntroductionToDOTNET/Snake/Program.cs
+++ IntroductionToDOTNET/Snake/Program.cs
@@ -10,12 +10,43 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
-			char key;
+			Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+			Random rand = new Random();
+			int x = rand.Next(Console.WindowWidth);
+			int y = rand.Next(Console.WindowHeight);
+			Console.CursorVisible = false;
+			ConsoleKey key;
 			do
 			{
-				key = Console.ReadKey(true).KeyChar;
-				Console.WriteLine($"{(int)key}\t{key}");
-			} while (true);
+				Console.Clear();
+				Console.SetCursorPosition(0, 0);
+				Console.ResetColor();
+				Console.WriteLine($"X = {x}\t Y = {y}");
+				Console.SetCursorPosition(x, y);
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine((char)2);
+				key = Console.ReadKey(true).Key;
+				//Console.WriteLine($"{(int)key}\t{key}");
+				switch (key)
+				{
+					case ConsoleKey.UpArrow:
+					case ConsoleKey.W: y--; break;
+					case ConsoleKey.DownArrow:
+					case ConsoleKey.S: y++; break;
+					case ConsoleKey.LeftArrow:
+					case ConsoleKey.A: x--; break;
+					case ConsoleKey.RightArrow:
+					case ConsoleKey.D: x++; break;
+				}
+				if (
+					x == 0 || y == 0 ||
+					x == Console.WindowWidth || y == Console.WindowHeight
+					) Console.Beep();
+				if (x == 0) x = 1;
+				if (x == Console.WindowWidth) x = Console.WindowWidth - 1;
+				if (y == 0) y = 1;
+				if (y == Console.WindowHeight) y = Console.WindowHeight - 1;
+			} while (key != ConsoleKey.Escape);
 		}
 	}
 }
