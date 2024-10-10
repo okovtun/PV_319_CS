6e1481923c0655234bd24eae382d3a9c68452a5b
User <User@DESKTOP-0TCDSR6>
2024-10-10 20:18:29 +0300

    Operators overloading

--- Encapsulation/Encapsulation/Program.cs
+++ Encapsulation/Encapsulation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define DISTANCE
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace Encapsulation
 	{
 		static void Main(string[] args)
 		{
+
+#if DISTANCE
 			Point A = new Point();
 			//Console.WriteLine($"X = {A.GetX()}\tY = {A.GetY()}");
 			//A.SetX(2000);
@@ -20,6 +23,27 @@ namespace Encapsulation
 			A.X = 22;
 			A.Y = 33;
 			A.Print();
+
+			//Point B = new Point();
+			Point B = new Point(A); //CopyConstructor
+			B.Print();
+
+			Point C;    //Это НЕ объект, а ссылка на объект
+			C = new Point(B);   //Объект можно создать только при помощи оператора 'new'
+			C.Print(); 
+#endif
+
+			Point A = new Point(2, 3);
+			Point B = new Point(7, 8);
+			Point C = new Point(A + B);
+			C.Print();
+			Point D = new Point(A - B);
+			D.Print();
+
+			for (Point i = new Point(); i.X < 10; i++)
+			{
+				i.Print();
+			}
 		}
 	}
 }
