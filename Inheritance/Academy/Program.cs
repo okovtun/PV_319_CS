2f9ac4e7857ce02fed49fae662e96b27890fd650
User <User@DESKTOP-0TCDSR6>
2024-10-10 21:55:36 +0300

    Inheritance & Files

---
+++ Inheritance/Academy/Program.cs
@@ -0,0 +1,34 @@
+﻿//#define INHERITANCE_CHECK
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+#if INHERITANCE_CHECK
+			Human human = new Human("Montana", "Antonio", 25);
+			human.Print();
+			Console.WriteLine(human);
+
+			Student student = new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 96);
+			student.Print();
+			Console.WriteLine(student);
+
+			Teacher teacher = new Teacher("White", "Walter", 50, "Chemistry", 25);
+			teacher.Print();
+			Console.WriteLine(teacher); 
+#endif
+			Human[] group = new Human[]
+				{
+					new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 96),
+					new Teacher("White", "Walter", 50, "Chemistry", 25)
+				};
+		}
+	}
+}
