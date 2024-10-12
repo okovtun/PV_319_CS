a682d184d5428e179e46cffca1ed96613f7ab10f
User <User@DESKTOP-0TCDSR6>
2024-10-12 21:11:19 +0300

    Print & Save DONE

---
+++ Inheritance/Academy/Graduate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	class Graduate:Student
+	{
+		public string Subject { get; set; }
+		public Graduate
+			(
+			string lastName, string firstName, int age,
+			string speciality, string group, double rating, double attendance,
+			string subject
+			) : base(lastName, firstName, age, speciality, group, rating, attendance)
+		{
+			Subject = subject;
+			Console.WriteLine($"GConstructor:\t{GetHashCode()}");
+		}
+		~Graduate()
+		{
+			Console.WriteLine($"GDestructor:\t{GetHashCode()}");
+		}
+		public override string ToString()
+		{
+			return base.ToString() + Subject;
+		}
+	}
+}
--- Inheritance/Academy/Program.cs
+++ Inheritance/Academy/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace Academy
 {
@@ -27,8 +29,31 @@ namespace Academy
 			Human[] group = new Human[]
 				{
 					new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 96),
-					new Teacher("White", "Walter", 50, "Chemistry", 25)
+					new Teacher("White", "Walter", 50, "Chemistry", 25),
+					new Graduate("Schreder", "Hank", 40, "Criminalystic", "DEA", 70, 40, "How to catch Heisenberg"),	//Drug Enforcement Administration
+					new Student("Vercetty", "Tommy", 30, "Theft", "Vice", 97, 98),
+					new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
 				};
+			Print(group);
+			Save(group, "group.txt");
+		}
+		static void Print(Human[] group)
+		{
+			for (int i = 0; i < group.Length; i++)
+			{
+				Console.WriteLine(group[i]);
+			}
+			Console.WriteLine();
+		}
+		static void Save(Human[] group, string filename)
+		{
+			StreamWriter sw = new StreamWriter(filename);
+			for (int i = 0; i < group.Length; i++)
+			{
+				sw.WriteLine(group[i]);
+			}
+			sw.Close();
+			Process.Start("notepad", filename);
 		}
 	}
 }
