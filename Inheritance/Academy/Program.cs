0bfec2e9d81bbb6369dc0298af543a10d3f31624
User <User@DESKTOP-0TCDSR6>
2024-10-12 21:48:24 +0300

    Write to CSV DONE

--- Inheritance/Academy/Program.cs
+++ Inheritance/Academy/Program.cs
@@ -34,26 +34,9 @@ namespace Academy
 					new Student("Vercetty", "Tommy", 30, "Theft", "Vice", 97, 98),
 					new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
 				};
-			Print(group);
-			Save(group, "group.txt");
+			Streamer.Print(group);
+			Streamer.Save(group, "group.csv");
 		}
-		static void Print(Human[] group)
-		{
-			for (int i = 0; i < group.Length; i++)
-			{
-				Console.WriteLine(group[i]);
-			}
-			Console.WriteLine();
-		}
-		static void Save(Human[] group, string filename)
-		{
-			StreamWriter sw = new StreamWriter(filename);
-			for (int i = 0; i < group.Length; i++)
-			{
-				sw.WriteLine(group[i]);
-			}
-			sw.Close();
-			Process.Start("notepad", filename);
-		}
+		
 	}
 }
---
+++ Inheritance/Academy/Streamer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Academy
+{
+	//Single responsibility principle
+	class Streamer
+	{
+		internal static void Print(Human[] group)
+		{
+			for (int i = 0; i < group.Length; i++)
+			{
+				Console.WriteLine(group[i]);
+			}
+			Console.WriteLine();
+		}
+		internal static void Save(Human[] group, string filename)
+		{
+			StreamWriter sw = new StreamWriter(filename);
+			for (int i = 0; i < group.Length; i++)
+			{
+				sw.WriteLine(group[i].ToFileString());
+			}
+			sw.Close();
+			Process.Start("excel", filename);
+		}
+		//CSV - Comma Separated Values (Значения, разделенные запятой).
+	}
+}
